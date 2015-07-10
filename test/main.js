8d2aa36e9a485105702265ce2652a16ad1b8cca6
damienmarchand <la_casserole_peinte@hotmail.fr>
2015-07-10 20:10:37 +0200

    test travis

--- test/main.js
+++ test/main.js
@@ -1,3 +1,15 @@
-/**
- * Created by Damien on 10/07/2015.
- */
+var should = require('should');
+var getJSONInformationIPv4 = require('../lib/main');
+
+describe('getJSONInformationIPv4', function () {
+   describe('With invalid IP', function () {
+     it('returns an error',function(){
+         getJSONInformationIPv4('',function(err, result){
+             should.exist(err);
+         })
+     });
+   });
+});
+
+
+
