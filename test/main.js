6ed42e864128a6fb8ac5f8b34108d565fe77d014
damienmarchand <la_casserole_peinte@hotmail.fr>
2015-07-10 23:35:51 +0200

    add sync request

--- test/main.js
+++ test/main.js
@@ -2,13 +2,22 @@ var should = require('should');
 var getJSONInformationIPv4 = require('../lib/main');
 
 describe('getJSONInformationIPv4', function () {
-   describe('With invalid IP', function () {
+   describe('With empty IP', function () {
      it('returns an error',function(){
          getJSONInformationIPv4('',function(err, result){
-             should.exist(err);
+             should.exists(err);
+             should.not.exists(result);
          })
      });
    });
+
+    describe('With Google IP', function () {
+        it('returns a result',function(){
+            getJSONInformationIPv4('64.233.167.94',function(err, result){
+                should.not.exists(err);
+            })
+        });
+    });
 });
 
 
