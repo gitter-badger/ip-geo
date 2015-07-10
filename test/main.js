564a9aaf224d5c9326ab70d1a82f8ee195b317cf
damienmarchand <la_casserole_peinte@hotmail.fr>
2015-07-11 00:05:47 +0200

    add object and change timeout to 6 sec by request for tests

--- test/main.js
+++ test/main.js
@@ -1,10 +1,10 @@
 var should = require('should');
-var getJSONInformationIPv4 = require('../lib/main');
+var getIPgeo = require('../lib/main');
 
 describe('getJSONInformationIPv4', function () {
    describe('With empty IP', function () {
      it('returns an error',function(){
-         getJSONInformationIPv4('',function(err, result){
+         getIPgeo.getJSONInformationIPv4('',function(err, result){
              should.exists(err);
              should.not.exists(result);
          })
@@ -12,9 +12,32 @@ describe('getJSONInformationIPv4', function () {
    });
 
     describe('With Google IP', function () {
+        this.timeout(6000);
         it('returns a result',function(){
-            getJSONInformationIPv4('64.233.167.94',function(err, result){
+            getIPgeo.getJSONInformationIPv4('64.233.167.94',function(err, result){
                 should.not.exists(err);
+                should.exists(result);
+            })
+        });
+    });
+});
+
+describe('Get IP information object',function(){
+    describe('With empty IP', function () {
+        it('returns an error',function(){
+            getIPgeo.getObjectInformationIPv4('',function(err, result){
+                should.exists(err);
+                should.not.exists(result);
+            })
+        });
+    });
+
+    describe('With Google IP', function () {
+        it('returns a result',function(){
+            this.timeout(6000);
+            getIPgeo.getObjectInformationIPv4('64.233.167.94',function(err, result){
+                should.not.exists(err);
+                should.exists(result);
             })
         });
     });
