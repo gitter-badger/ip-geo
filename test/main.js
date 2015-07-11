c67c1b971de59ee6d1ba46e3da3a8df7d15ecc48
damienmarchand <la_casserole_peinte@hotmail.fr>
2015-07-11 18:25:58 +0200

    add tests

--- test/main.js
+++ test/main.js
@@ -1,8 +1,8 @@
 var should = require('should');
 var getIPgeo = require('../lib/main');
 
-describe('Get IP information ',function(){
-    describe('With empty IP', function () {
+describe('Get IPV4 information ',function(){
+    describe('With empty IPV4', function () {
         it('returns an error',function(){
             getIPgeo.getInformationIPv4('',function(err, result){
                 should.exists(err);
@@ -11,9 +11,29 @@ describe('Get IP information ',function(){
         });
     });
 
-    describe('With invalid IP', function () {
+    describe('With invalid IPV4', function () {
         it('returns an error',function(){
             getIPgeo.getInformationIPv4('23.ezr.4z.e',function(err, result){
+                should.exists(err);
+                should.not.exists(result);
+            })
+        });
+    });
+});
+
+describe('Get IPV6 information ',function(){
+    describe('With empty IPV6', function () {
+        it('returns an error',function(){
+            getIPgeo.getInformationIPv6('',function(err, result){
+                should.exists(err);
+                should.not.exists(result);
+            })
+        });
+    });
+
+    describe('With invalid IPV6', function () {
+        it('returns an error',function(){
+            getIPgeo.getInformationIPv6('23.ezr.4z.e',function(err, result){
                 should.exists(err);
                 should.not.exists(result);
             })
